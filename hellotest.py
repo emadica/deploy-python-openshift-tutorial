f4f26b2ec7b94cec32b33391b34dcde26147cb24
Student User <student@workstation.lab.example.com>
2023-09-10 12:09:13 -0400

    test

---
+++ hellotest.py
@@ -0,0 +1,15 @@
+'''
+Created on 04-Sep-2019
+
+'''
+
+# -*- coding: UTF-8 -*-
+"""
+hello_test: First Python-Flask webapp
+"""
+def main():
+    """Test hello"""
+    return 'Hello, world!'
+
+if __name__ == '__main__':  # Script executed directly?
+    print("Hello Test! Built with a BuildHook")
