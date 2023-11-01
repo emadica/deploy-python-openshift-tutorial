7e3a748a4786bdf94cf07289698bd633fa2d3c40
Student User <student@workstation.lab.example.com>
2023-11-01 12:58:23 -0400

    test2

--- perx/hellotest.py
+++ perx/hellotest.py
@@ -4,6 +4,7 @@ Created on 04-Sep-2019
 '''
 
 # -*- coding: UTF-8 -*-
+import sys
 """
 hello_test: First Python-Flask webapp
 """
@@ -12,4 +13,4 @@ def main():
     return 'Hello, world!'
 
 if __name__ == '__main__':  # Script executed directly?
-    print("Hello Test! Built with a BuildHook")
+    print("Hello Test! Built with a BuildHook " + sys.argv[1])
